a1b9d6ce93f59b02cc1993440776ad58bcf2cc5f
39366 <39366@10.0.86.130>
2019-05-15 18:37:49 +0200

    prima modifica 15/05

--- src/it/unibs/fp/cartaAlta/Gioatore.java
+++ src/it/unibs/fp/cartaAlta/Gioatore.java
@@ -2,4 +2,36 @@ package it.unibs.fp.cartaAlta;
 
 public class Gioatore {
 
+	private static final int PUNTATA_MINIMA = 1;
+	private static final int PATRIMONIO_MINIMO = 0;
+
+	private String nome;
+	private int patrimonio;
+
+	public boolean puoGiocare() {
+		return patrimonio > PATRIMONIO_MINIMO;
+	}
+
+	public boolean punta(int puntata) {
+		if (puntata >= PUNTATA_MINIMA && puntata <= patrimonio) {
+			patrimonio -= puntata;
+			return true;
+		}
+		return false;
+	}
+
+	public void accredita(int accredito) {
+		patrimonio += accredito;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public int getPatrimonio() {
+		return patrimonio;
+	}
+	
+	
+
 }
--- src/it/unibs/fp/cartaAlta/Partita.java
+++ src/it/unibs/fp/cartaAlta/Partita.java
@@ -1,5 +1,29 @@
 package it.unibs.fp.cartaAlta;
 
-public class Partita {
+import it.unibs.fp.mylib.*;
 
+public class Partita {
+	private static final String RICHIESTA_CONTINUA = "Vuoi continuare a giocare? ";
+	private Mazzo mazzo;
+	private Gioatore player;
+	
+	public Partita(Mazzo mazzo, Gioatore player) {
+		this.mazzo = mazzo;
+		this.player = player;
+	}
+	
+	public void play() {
+		
+		do {
+			Carta cartaGiocatore = mazzo.estrai();
+			Carta cartaMazzo = mazzo.estrai();
+			System.out.println(player.getNome()+": "+cartaGiocatore.toString());
+			System.out.println("Banco: " + cartaMazzo.toString());
+		}while(InputDati.yesOrNo(RICHIESTA_CONTINUA) && player.puoGiocare());
+		
+	}
+	
+	public void punta() {
+		
+	}
 }
