dfe615c456042465e832123339290c717f3fafae
bagli <bagli@10.0.89.65>
2019-05-15 17:44:45 +0200

    Correzione classe mazzo

--- src/it/unibs/fp/cartaAlta/Mazzo.java
+++ src/it/unibs/fp/cartaAlta/Mazzo.java
@@ -4,16 +4,11 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 public class Mazzo {
-	private String seme[];
-	private String nome[];
-	private int val[];
+
 	private ArrayList<Carta> c = new ArrayList<Carta>();
 
 	public Mazzo(String[] seme, String[] nome, int[] val) {
-		this.seme = seme;
-		this.nome = nome;
-		this.val = val;
-
+		
 		for (int i = 0; i < seme.length; i++) {
 			for (int j = 0; j < nome.length; j++) {
 				c.add(new Carta(seme[i], nome[j], val[j]));
